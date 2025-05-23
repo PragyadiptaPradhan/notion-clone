4ac8f9158288a2329008500d6a89da186e2c283f
Hrisi Pradhan <pragyadipta84@gmail.com>
2025-05-23 08:41:22 +0530

    first commit

---
+++ app/(marketing)/_components/heroes.tsx
@@ -0,0 +1,41 @@
+import Image from "next/image";
+
+export const Heroes = () => {
+    return (
+      <div className="flex flex-col items-center justify-center max-w-5xl">
+        <div className="flex item-center">
+          <div
+            className="relative w-[300px] h-[300px] sm:w-[350px] 
+                sm:h-[350px] md:h-[400px] md:w-[400px]"
+          >
+            <Image
+              src="/idea.svg"
+              fill
+              className="object-contain dark:hidden"
+              alt="Idea"
+            />
+            <Image
+              src="/idea-dark.svg"
+              fill
+              className="hidden object-contain dark:block"
+              alt="Idea"
+            />
+          </div>
+          <div className="relative  h-[400px] w-[400px] hidden md:block">
+            <Image
+              src="/team.svg"
+              fill
+              className="object-contain dark:hidden"
+              alt="Team"
+            />
+            <Image
+              src="/team-dark.svg"
+              fill
+              className="hidden object-contain dark:block"
+              alt="Team"
+            />
+          </div>
+        </div>
+      </div>
+    );
+}
